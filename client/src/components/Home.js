2c6c4dd9e249ffe273e936245afd485697036dd8
xshirl <xshirl525@gmail.com>
2020-08-08 15:08:08 -0400

    styled search bar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -16,7 +16,7 @@ export default class Home extends Component {
 
         <Search />
         <EventForm />
-        <PublicEventList />
+        {/* <PublicEventList /> */}
       </div>
     );
   }
